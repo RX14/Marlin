468e4cf7320fc504f4d667892d269d1a428c4594
RX14 <steph@rx14.co.uk>
2020-03-18 13:35:42 +0000

    Reduce X motor current to 850mA

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2022,7 +2022,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       900        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       850        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     64    // 0..256
     #define X_RSENSE          0.11
